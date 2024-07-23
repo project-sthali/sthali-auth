241eb6d432d8e433c8eb619bc05daa68849f26bf
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-07-23 19:57:14 -0300

    wip

--- src/sthali_auth/__init__.py
+++ src/sthali_auth/__init__.py
@@ -1,6 +1,77 @@
 """This module provides security components."""
-from .dependencies import http_basic
+
+from collections.abc import Callable
+from contextlib import asynccontextmanager
+from logging import info
+from typing import Any, Literal
+
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+
+from sthali_crud import AppSpecification as CRUDAppSpecification
+from sthali_crud import SthaliCRUD, load_and_parse_spec_file
+
+from .dependencies import APIKeyAuth, APIKey
+
+api_router = APIRouter()
+
+
+class Auth(BaseModel):
+    method: Literal["api_key_header"]
+
+
+class Authorize(BaseModel):
+    service: str
+    resource: str
+    auth: Auth
+
+
+@api_router.post("/authorize")
+async def authorize(authorize: Authorize) -> None:
+    print("authorize")
+    print(authorize.model_dump_json())
+    return
+
+
+@dataclass
+class AppSpecification:
+    """Represents the specification of a SthaliAuth application.
+
+    Attributes:
+        service (ServiceSpecification): {...}.
+        crud (CRUDAppSpecification): Represents the specification of a SthaliCRUD application.
+    """
+
+    # service: Annotated[ServiceSpecification, Field(description="...")]
+    crud: CRUDAppSpecification
+
+
+@asynccontextmanager
+async def default_lifespan(app: FastAPI):
+    """A context manager that handles the startup and shutdown of SthaliAuth.
+
+    Args:
+        app (FastAPI): The FastAPI application instance.
+
+    Yields:
+        None
+    """
+    info("Startup SthaliAuth")
+    yield
+    info("Shutdown SthaliAuth")
+
+
+class SthaliAuth(SthaliCRUD):
+    def __init__(self, app_spec: AppSpecification, lifespan: Callable[..., Any] = default_lifespan) -> None:
+        super().__init__(app_spec.crud, lifespan)
+        self.app.include_router(api_router)
+
 
 __all__ = [
-    "http_basic",
+    "APIKeyAuth",
+    "APIKey",
+    "AppSpecification",
+    "SthaliAuth",
+    "load_and_parse_spec_file",
 ]
--- src/sthali_auth/dependencies.py
+++ src/sthali_auth/dependencies.py
@@ -1,7 +1,54 @@
-"""This module provides the dependencies for sthali-backend usage."""
-from typing import Annotated
+from typing import Annotated, Any, Literal
 
-from fastapi import Depends
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, Request
+from fastapi.security import APIKeyCookie, APIKeyHeader, APIKeyQuery
+from httpx import post
+from pydantic.dataclasses import dataclass
 
-http_basic: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]
+
+class ServiceClient:
+    def __init__(self) -> None:
+        self.url = "localhost:8000/authorize"
+
+    def call(self, headers: dict[str, Any], json: dict[str, Any]) -> None:
+        post(self.url, headers=headers, json=json)
+
+
+@dataclass
+class APIKey:
+    name: str
+    scheme_name: str | None = None
+    description: str | None = None
+    auto_error: bool = True
+
+
+@dataclass
+class APIKeyScheme:
+    header = APIKeyHeader
+    cookie = APIKeyCookie
+    query = APIKeyQuery
+
+
+class APIKeyAuth:
+    client = ServiceClient()
+
+    def __init__(self, api_key: APIKey, type: Literal["header", "cookie", "query"], service: str) -> None:
+        scheme = getattr(APIKeyScheme, type)
+        self.type = type
+        self.service = service
+        self.scheme = scheme(
+            name=api_key.name,
+            scheme_name=api_key.scheme_name,
+            description=api_key.description,
+            auto_error=api_key.auto_error,
+        )
+
+    @property
+    def dependency(self):
+        def api_key_auth(key: Annotated[str, Depends(self.scheme)], request: Request):
+            print("api_key_auth")
+            headers = {"X-API-Key": key}
+            json = {"service": self.service, "resource": request.url.path, "auth": {"": f"api_key_{self.type}"}}
+            self.client.call(headers=headers, json=json)
+
+        return Depends(api_key_auth)
